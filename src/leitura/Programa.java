5b1927fbbaa338d88efb97694e386ac3214ab5f9
Robertosousa <robertosousa186@gmail.com>
2019-11-30 01:31:54 -0300

    Código mais limpo

--- src/leitura/Programa.java
+++ src/leitura/Programa.java
@@ -1,21 +1,14 @@
 package leitura;
 
 import java.io.*;
-import java.io.IOException;
-import java.util.*;
 
 public class Programa {
 
 	public static void main(String[] args) {
 
 		String path = new String("c:\\temp\\in.txt");
-		FileReader fr = null;
-		BufferedReader br = null;
 
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String line = br.readLine();
 			while (line != null) {
 				System.out.println(line);
@@ -25,19 +18,6 @@ public class Programa {
 		} catch (IOException e) {
 			System.out.println("Erro: " + e.getMessage());
 
-		} finally {
-			try {
-				if (br != null) {
-				}
-				br.close();
-
-				if (fr != null) {
-					fr.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 	}
-
 }
